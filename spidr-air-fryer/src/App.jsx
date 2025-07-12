014ab09c68510da84614f7acd5445aa17e29f3bf
sathwikasuddala <sathwika.suddala@gmail.com>
2025-07-12 01:41:03 -0700

    Initial commit

---
+++ spidr-air-fryer/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AirFryerInterestForm from "./AirFryerInterestForm";
+import "./App.css";
+
+function App() {
+ return ( 
+ <>
+ <img src="/spidr-logo.png" alt="Spidr Logo" className="fixed-logo" />
+ <img src="/spidr-title.png" alt="Spidr Title" className="fixed-title"/>
+ <AirFryerInterestForm />
+ </>
+ );
+}
+
+export default App
