d814e542ce6c97f65ee9676c7ff3f0e8c13858a3
sathwikasuddala <sathwika.suddala@gmail.com>
2025-07-15 21:40:02 -0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import "./App.css";
 function App() {
  return ( 
  <>
- <img src={`${import.meta.env.BASE_URL}spidr-logo.png`} alt="Spidr Logo" />
+ <img src='/spidr-air-fryer/spidr-logo.png' alt="Spidr Logo"  className="fixed-logo" />
+<img src='/spidr-air-fryer/spidr-title.png' alt="Spidr Title"  className="fixed-title" />
 
-<img src={`${import.meta.env.BASE_URL}spidr-title.png`} alt="Spidr Title" />
  <AirFryerInterestForm />
  </>
  );
